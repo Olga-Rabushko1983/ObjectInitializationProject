9a20d1f617d9189b02d57b28378a6e77c997c6f4
Olga-Rabushko1983 <oiushka1@mail.ru>
2022-06-30 20:39:37 +0300

    add copy constructor with params

--- src/Student.java
+++ src/Student.java
@@ -3,36 +3,28 @@ public class Student {
     public int age;
     public double mark;
     public boolean alive;
-
+//defalt constructor
     public Student() {
         name = "no name";
         age = 16;
         mark = 4;
         alive = true;
     }
-
+//full constructor with param
     public Student(String n, int a,double m, boolean al){
         name=n;
         age=a;
         mark=m;
         alive=al;
     }
-    public Student(String n){
-        name=n;
+    public Student(Student student){
+        name=student.name;
+        age=student.age;
+        mark=student.mark;
+        alive=student.alive;
 
     }
-    public Student(double m){
-        mark=m;
-    }
-    public Student(String n, double m){
-        name =n;
-        mark=m;
-    }
-    public Student( double m, String n){
-        mark=m;
-        name =n;
 
-    }
     public String getInfo() {
         return name + ":age=" + age + ",mark=" + mark + ",is live=" + (alive ? "yes" : "no");
 
